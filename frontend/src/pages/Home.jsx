19c48f2314a8d2cb29b6a439d2d7139eaf48ef2a
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-03-17 01:51:03 -0400

    Change work1 image format from PNG to JPEG

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,13 +12,14 @@ import womenHaircut from "../assets/work2.png";
 import menHaircut from "../assets/men.jpg";
 import waxingEyebrows from "../assets/waxing.png";
 import kids from "../assets/kids2.jpg";
-import work1 from "../assets/work1.png";
+import work1 from "../assets/work1.jpeg";
 import work2 from "../assets/work2.png";
 import work3 from "../assets/work3.png";
 import work4 from "../assets/work4.png";
 import work5 from "../assets/work5.png";
 import work6 from "../assets/work6.png";
 import work7 from "../assets/work7.png";
+import work8 from "../assets/women-cut.jpeg";
 
 
 /* ---------------------- Fade in/out Offer Rotator ---------------------- */
